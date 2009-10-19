b3d16745a81363ddfbf9a4e6ea54ce2e07187a50
Daniel Cordero <theappleman@gmail.com>
2009-10-19 12:48:17 +0059

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -5,9 +5,11 @@ static gchar *stylefile      = ".surf/style.css";
 static gchar *scriptfile     = ".surf/script.js";
 static gchar *cookiefile     = ".surf/cookie.txt";
 static gchar *dldir          = ".surf/dl";
+static time_t sessiontime    = 31557600;
+
 #define MODKEY GDK_CONTROL_MASK
 static Key keys[] = {
-    /* modifier	            keyval      function    arg             Focus */
+    /* modifier             keyval      function    arg             Focus */
     { MODKEY|GDK_SHIFT_MASK,GDK_r,      reload,     { .b = TRUE },  Any },
     { MODKEY,               GDK_r,      reload,     { .b = FALSE }, Any },
     { MODKEY,               GDK_g,      showurl,    { 0 },          Any },
@@ -15,15 +17,15 @@ static Key keys[] = {
     { 0,                    GDK_Escape, hidesearch, { 0 },          Any },
     { 0,                    GDK_Escape, hideurl,    { 0 },          Any },
     { MODKEY|GDK_SHIFT_MASK,GDK_p,      print,      { 0 },          Any },
-    { MODKEY,               GDK_p,      clipboard,  { .b = TRUE },  Browser },
-    { MODKEY,               GDK_y,      clipboard,  { .b = FALSE }, Browser },
-    { MODKEY|GDK_SHIFT_MASK,GDK_j,      zoom,       { .i = -1 },    Browser },
-    { MODKEY|GDK_SHIFT_MASK,GDK_k,      zoom,       { .i = +1 },    Browser },
-    { MODKEY|GDK_SHIFT_MASK,GDK_i,      zoom,       { .i = 0  },    Browser },
-    { MODKEY,               GDK_l,      navigate,   { .i = +1 },    Browser },
-    { MODKEY,               GDK_h,      navigate,   { .i = -1 },    Browser },
-    { MODKEY,               GDK_j,      scroll,     { .i = +1 },    Browser },
-    { MODKEY,               GDK_k,      scroll,     { .i = -1 },    Browser },
+    { MODKEY,               GDK_c,      clipboard,  { .b = TRUE },  Browser },
+    { MODKEY,               GDK_v,      clipboard,  { .b = FALSE }, Browser },
+    { MODKEY,               GDK_minus,  zoom,       { .i = -1 },    Browser },
+    { MODKEY|GDK_SHIFT_MASK,GDK_plus,   zoom,       { .i = +1 },    Browser },
+    { MODKEY,               GDK_0,      zoom,       { .i = 0  },    Browser },
+    { MODKEY,               GDK_Left,   navigate,   { .i = -1 },    Browser },
+    { MODKEY,               GDK_Right,  navigate,   { .i = +1 },    Browser },
+    { MODKEY,               GDK_Up,     scroll,     { .i = +1 },    Browser },
+    { MODKEY,               GDK_Down,   scroll,     { .i = -1 },    Browser },
     { 0,                    GDK_Escape, stop,       { 0 },          Browser },
     { MODKEY,               GDK_o,      source,     { 0 },          Browser },
     { MODKEY,               GDK_n,      searchtext, { .b = TRUE },  Browser|SearchBar },
